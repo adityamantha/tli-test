ecda3bcfd20e95b5a8166ad39fd9b9cdcf9132b0
Aditya Mantha <adityamantha@Adityas-MacBook-Pro.local>
2019-04-28 15:15:30 -0400

    Removed the seed data from test

--- api/server.js
+++ api/server.js
@@ -28,8 +28,8 @@ mongoose
   .then(async () => {
     console.log(`Connected to database at: ${URL}`);
     try {
-      await require('./utils/seed').truncate();
-      await require('./utils/seed').seed();
+      // await require('./utils/seed').truncate();
+      // await require('./utils/seed').seed();
 
       server.listen(PORT, () => {
         console.log(`Server is running on PORT:${PORT}`);
