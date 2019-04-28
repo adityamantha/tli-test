3c84fe1643d73a86339f875fb3e969ad17f315e2
Aditya Mantha <adityamantha@Adityas-MacBook-Pro.local>
2019-04-28 14:11:59 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Login from './components/login/Login';
-import Post from './components/post/Post';
-import Feed from './components/feed/Feed';
+import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Feed />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
