fe17d1160b3a54008e880c9b76354faaa6dc92b3
Aditya Mantha <adityamantha@Adityas-MacBook-Pro.local>
2019-04-28 14:47:25 -0400

    Added Api middleware and server.js for deployment

---
+++ api/server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const express = require('express');
+const http = require('http');
+
+const router = express();
+
+const { applyMiddleware } = require('./utils');
+const middleWare = require('./middleware');
+const errorHandlers = require('./middleware/errorHandlers.js');
+
+// const { router: cohortRoutes } = require('./routes/cohorts/cohortRoutes');
+// const { router: userRoutes } = require('./routes/users/userRoutes');
+const { PORT, URL } = require('./utils/constants');
+
+applyMiddleware(middleWare, router);
+
+// router.use('/api/cohorts', cohortRoutes);
+// router.use('/api/users', userRoutes);
+
+applyMiddleware(errorHandlers, router);
+
+const server = http.createServer(router);
+
+mongoose
+  .connect(URL, { useNewUrlParser: true })
+  .then(async () => {
+    console.log(`Connected to database at: ${URL}`);
+    try {
+      await require('./utils/seed').truncate();
+      await require('./utils/seed').seed();
+
+      server.listen(PORT, () => {
+        console.log(`Server is running on PORT:${PORT}`);
+        if (process.send) {
+          // NOTE: process is being run by pm2
+          process.send('ready');
+        }
+      });
+    } catch (e) {
+      console.error(`Error starting server: ${e}`);
+      throw e;
+    }
+  });
---
+++ api/utils/constants.js
@@ -0,0 +1,6 @@
+'use strict';
+
+exports.URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
+exports.PORT = process.env.PORT || 3001;
+exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
+exports.PUBLIC_FOLDER = (process.env.NODE_ENV === 'production') ? 'build' : 'public';
